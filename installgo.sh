b99cad8c6c5d6e640a646e01f610cd81fffaed51
Nic Prettejohn <nicolas.prettejohn13@imperial.ac.uk>
2014-11-07 10:33:19 +0000

    Fix typo export -> echo

--- installgo.sh
+++ installgo.sh
@@ -4,8 +4,8 @@ if [ -n "`which apt-get`" ];
 then
     mkdir $HOME/go
     echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
-    export 'export PATH=$PATH:$GOPATH/bin' >> ~/.bash_profile
-    export 'export GOROOT=/usr/lib/go' >> ~/.bash_profile
+    echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bash_profile
+    echo 'export GOROOT=/usr/lib/go' >> ~/.bash_profile
     apt-get install -y golang-go
     source ~/.bash_profile
     go get github.com/blynn/nex
