d77b236c5c2fa49feb9cdd0010c13554fb9e2850
Nic Prettejohn <nicolas.prettejohn13@imperial.ac.uk>
2014-11-07 20:42:34 +0000

    Remove useless exports

--- installgo.sh
+++ installgo.sh
@@ -3,10 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 if [ -n "`which apt-get`" ];
 then
     mkdir $HOME/go
-    echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
-    echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bash_profile
-    echo 'export GOROOT=/usr/lib/go' >> ~/.bash_profile
     apt-get install -y golang-go
-    source ~/.bash_profile
-    go get github.com/blynn/nex
+    GOPATH=$HOME/go go get github.com/blynn/nex
 fi
