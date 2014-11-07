725d83aac69f4c75546493b4371e3a41427a14c0
Nic Prettejohn <nicolas.prettejohn13@imperial.ac.uk>
2014-11-07 10:21:36 +0000

    Install nex in the installgo script

--- installgo.sh
+++ installgo.sh
@@ -2,8 +2,10 @@ export DEBIAN_FRONTEND=noninteractive
 if [ -n "`which apt-get`" ];
 then
     mkdir $HOME/go
-    export GOPATH=$HOME/go
-    export PATH=$PATH:$GOPATH/bin
-    export GOROOT=/usr/lib/go
+    echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
+    export 'export PATH=$PATH:$GOPATH/bin' >> ~/.bash_profile
+    export 'export GOROOT=/usr/lib/go' >> ~/.bash_profile
     apt-get install -y golang-go
+    source ~/.bash_profile
+    go get github.com/blynn/nex
 fi
