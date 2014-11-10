b379427894cd46bfc1cd74d41b85d76ba5c74deb
David Avedissian <david.avedissian13@imperial.ac.uk>
2014-11-10 10:10:18 +0000

    Checks for go installation

--- installgo.sh
+++ installgo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-if [ -n "`which apt-get`" ];
+if [ -n "`which apt-get`" ] && [ -z "`which go`" ];
 then
     mkdir $HOME/go
     apt-get install -y golang-go
