293f8e8327de475c0fbfa450f0b2ecc0e90161c5
James Grant <jamescolin.grant@gmail.com>
2014-11-13 21:07:11 +0000

    Moved errors functions into their own file

---
+++ src/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+const SYNTAX_ERROR = 100
+const SEMANTIC_ERROR = 200
+
+func SyntaxError(lineNo int, s string, a... interface{}) {
+    errorStr := fmt.Sprintf(s, a...)
+    fmt.Printf("Line %d: %s\n", lineNo, errorStr)
+    exitFlag = SYNTAX_ERROR
+}
+
+func SemanticError(lineNo int, s string, a... interface{}) {
+    errorStr := fmt.Sprintf(s, a...)
+    fmt.Printf("Line %d: %s\n", lineNo, errorStr)
+    exitFlag = SEMANTIC_ERROR
+}
+
