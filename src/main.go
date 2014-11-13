843e60c444bef9304b933f9af5842fa2121298b6
James Grant <jamescolin.grant@gmail.com>
2014-11-13 21:10:58 +0000

    gofmt all the things

--- src/main.go
+++ src/main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-    "flag"
-    "os"
-    "fmt"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-    enableDebug := flag.Bool("d", false, "Enable debug mode")
-    flag.Parse()
+	enableDebug := flag.Bool("d", false, "Enable debug mode")
+	flag.Parse()
 
-    if *enableDebug {
-    yyDebug = 20
-    }
+	if *enableDebug {
+		yyDebug = 20
+	}
 
-    // Parse the code, build the AST using the yacc file, and syntax-check.
-    // We tend to think of the first line as line 1, not line 0
-    lex = NewLexerWithInit(os.Stdin, func(l *Lexer){ l.l = 1 })
-    yyParse(lex)
-    if exitFlag != 0 {
-    os.Exit(exitFlag)
-    }
+	// Parse the code, build the AST using the yacc file, and syntax-check.
+	// We tend to think of the first line as line 1, not line 0
+	lex = NewLexerWithInit(os.Stdin, func(l *Lexer) { l.l = 1 })
+	yyParse(lex)
+	if exitFlag != 0 {
+		os.Exit(exitFlag)
+	}
 
-    // Semantic-check the tree
-    VerifySemantics(top.Stmt.(*ProgStmt))
-    if exitFlag != 0 {
-    os.Exit(exitFlag)
-    }
+	// Semantic-check the tree
+	VerifySemantics(top.Stmt.(*ProgStmt))
+	if exitFlag != 0 {
+		os.Exit(exitFlag)
+	}
 
-    // Print the representation of the tree
-    fmt.Println("Compilation successful!")
-    fmt.Println(top.Stmt.Repr())
+	// Print the representation of the tree
+	fmt.Println("Compilation successful!")
+	fmt.Println(top.Stmt.Repr())
 }
